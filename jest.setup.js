5742ee184836e1bd8f9d18395ddc927abb083c51
Sen_Reaksa <reaksasen804@gmail.com>
2024-07-22 10:21:18 +0700

    remove

--- jest.setup.js
+++
@@ -1,7 +0,0 @@
-process.env.AWS_ACCESS_KEY_ID = 'AKIA47CRVMAM6UZJMOUL';
-process.env.AWS_SECRET_ACCESS_KEY = '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';
-process.env.AWS_REGION = 'us-east-1';
-process.env.MONGODB_URI = 'mongodb+srv://reaksa:sLnhT5hPNnnPZK5a@cluster0.gqgsjrl.mongodb.net/dbconnect';
-// Add any other required environment variables
-
-require('dotenv').config({ path: '.env.test' });
